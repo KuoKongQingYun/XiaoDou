ef040d58ec1be07391604a3e12d4948d94be8837
Zhihao Lin <609898048@qq.com>
2015-05-15 17:46:22 +0800

    备份进度
    备份进度

--- themes/xiaodou/assets/js/ajax.js
+++ themes/xiaodou/assets/js/ajax.js
@@ -13,34 +13,7 @@ function sendAjaxRequest(url, target, backcall) {
     XMLHttpReq.onreadystatechange = function() {
         if (XMLHttpReq.readyState == 4) {
             if (XMLHttpReq.status == 200) {
-                var text = XMLHttpReq.responseText;
-                var cp = document.getElementById(target);
-                cp.innerHTML = text;
-                backcall();
-            }
-        }
-    }; //指定响应函数
-    XMLHttpReq.send(null);
-}
-function sendAjaxRequestAdd(url, target, backcall) {
-    var XMLHttpReq;
-    try {
-        XMLHttpReq = new ActiveXObject("Msxml2.XMLHTTP"); //IE高版本创建XMLHTTP
-    } catch (E) {
-        try {
-            XMLHttpReq = new ActiveXObject("Microsoft.XMLHTTP"); //IE低版本创建XMLHTTP
-        } catch (E) {
-            XMLHttpReq = new XMLHttpRequest(); //兼容非IE浏览器，直接创建XMLHTTP对象
-        }
-    }
-    XMLHttpReq.open("post", url, true);
-    XMLHttpReq.onreadystatechange = function() {
-        if (XMLHttpReq.readyState == 4) {
-            if (XMLHttpReq.status == 200) {
-                var text = XMLHttpReq.responseText;
-                var cp = document.getElementById(target);
-                cp.innerHTML = cp.innerHTML + text;
-                backcall();
+                backcall(XMLHttpReq.responseText);
             }
         }
     }; //指定响应函数
